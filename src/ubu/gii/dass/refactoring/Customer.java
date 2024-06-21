7c371b38c41c8ab93903eb25ce3c80f4eff6dc5f
xvs1001 <xvs1001@alu.ubu.es>
2024-06-21 19:24:17 +0200

    Refactored with renamed variable

--- src/ubu/gii/dass/refactoring/Customer.java
+++ src/ubu/gii/dass/refactoring/Customer.java
@@ -37,16 +37,16 @@ public class Customer {
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
-			Rental each = rentals.next();
-			thisAmount = amounted(thisAmount, each);	
+			Rental hire = rentals.next();
+			thisAmount = amounted(thisAmount, hire);	
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
-			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-					&& each.getDaysRented() > 1)
+			if ((hire.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&& hire.getDaysRented() > 1)
 				frequentRenterPoints++;
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t"
+			result += "\t" + hire.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
 		}
